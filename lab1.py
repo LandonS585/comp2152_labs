f0b6bc2d5ff71ed6fb9adf634d6ae1121bba824b
Landon <landon5855@gmail.com>
2026-01-19 00:02:17 -0500

    Add lab 1

---
+++ lab1.py
@@ -0,0 +1,24 @@
+#Sample Coding Question 01 Week 01
+#Landon Swales 
+
+#Defining Variables:
+variables = [1, 4, 7, 9]
+
+#Order of Operations:
+a = 1
+b = 2
+c = 3
+d = 4
+e = (a - ((b ** c) // d)) + (a % c)
+
+
+#Formatting:
+temperature = 32.6
+
+print("The temperature today is: " + temperature + " degrees Celsuis")
+
+#Common Functions:
+#userAge = int(input("Enter your age: "))
+#userAge = userAge + 22
+
+#print("Now showing the shop items filtered by age:", userAge)
